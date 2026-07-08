6c946a1be5f3fd3bde51241aa8ef7941fb7a5bda
Aslı <asli@Asl-MacBook-Air.local>
2026-07-08 20:10:57 +0300

    refactor(utils): use minkowski distance

--- utils/distances.py
+++ utils/distances.py
@@ -1,4 +1,14 @@
+def minkowski(a, b, p):
+    d_x = b[0] - a[0]
+    d_y = b[1] - a[1]
+
+    distance = (abs(d_x)**p + abs(d_y)**p)**(1/p)
+    return distance
+
+
 def manhattan(a, b):
-    d_x = abs(b[0] - a[0])
-    d_y = abs(b[1] - a[1])
-    return d_x + d_y
+    return minkowski(a, b, 1)
+
+
+def euclidean(a, b):
+    return minkowski(a, b, 2)
